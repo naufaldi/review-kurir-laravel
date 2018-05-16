f38762d30f4676d265c1c570ff1cf87153a659de
Bill Tanthowi Jauhari <xcodebill@gmail.com>
2018-05-16 08:46:13 +0700

    ready to production

--- resources/views/ongkir/create.blade.php
+++ resources/views/ongkir/create.blade.php
@@ -101,7 +101,7 @@
               <td>@{{ origin_details.city_name }}</td>
               <td>@{{ results[0].name }}</td>
               <td>@{{ cost.service }}</td>
-              <td>Rp. @{{ cost.cost[0].value }} Hari</td>
+              <td>Rp. @{{ cost.cost[0].value }}</td>
               <td>@{{ cost.cost[0].etd }} Hari</td>
             </tr>
           </tbody>
