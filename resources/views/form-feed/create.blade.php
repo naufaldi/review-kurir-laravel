3a8d30d0651c359b255d8c7fa6c75d29d844e60b
Bill Tanthowi jauhari <bill.tanthowi.j@gmail.com>
2018-10-11 17:00:45 +0700

    add form and feed

---
+++ resources/views/form-feed/create.blade.php
@@ -0,0 +1,47 @@
+                <div class="field">
+                    {{ Form::open(['route' =>'review.store', 'method' => 'POST']) }}
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="control">
+                        <div class="columns">
+                            <div class="column">
+                                <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="control">
+                        <div class="columns">
+                        <div class="column">
+                            <div class="select is-primary">
+                            <select name="kurir">
+                                @foreach($kurirs as $key => $kurir)
+                                    <option value="{{ $kurir->id }}">{{ $kurir->nama }}</option>
+                                @endforeach
+                            </select>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="select is-primary">
+                                <select name="rate">
+                                    <option value="1">1 star</option>
+                                    <option value="2">2 stars</option>
+                                    <option value="3">3 stars</option>
+                                    <option value="4">4 stars</option>
+                                    <option value="5">5 stars</option>
+                                
+                                </select>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <button class="button is-primary">Review</button>
+                        </div>
+                        </div>
+                         
+                    </div>
+                    </div>
+                    {{ Form::close() }}
+                </div>   
+                    
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    {{ Form::open(['route' =>'review.store', 'method' => 'POST']) }}
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    <div class="columns">
-                       <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
-                    </div>
-                    <div class="columns">
-                        <div class="column">
-                            <select name="kurir">
-                                @foreach($kurirs as $key => $kurir)
-                                    <option value="{{ $kurir->id }}">{{ $kurir->nama }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="column">
-                            <button class="button is-primary">Review</button>
-                        </div>
-                    </div>
-                    <div class="columns">
-                        <div class="column">
-                        </div>
-                    </div>
-                    {{ Form::close() }}
-                </div>
-            </div>
+<div class="is-four-fifths">
+    <div class="columns">
+        <div class="column">
+          @include('form-feed.create')  
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
+            @include('form-feed.index')
         </div>
     </div>
 </div>
